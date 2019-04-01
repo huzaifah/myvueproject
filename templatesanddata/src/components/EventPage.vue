33c753fcad52c3f9b5739f227e8e69f850132f0d
Huzaifah Dzulkifli <huzaifahd@hotmail.com>
2019-04-01 19:15:23 +0800

    Added mouse event

---
+++ templatesanddata/src/components/EventPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container-fluid">
+    <h3 class="bg-primary text-white text-center mt-2 p-2">{{ message }}</h3>
+    <table class="table table-sm table-striped table-bordered">
+      <thead>
+        <td>Index</td>
+        <td>Name</td>
+        <td>Action</td>
+      </thead>
+      <tr v-for="(name, index) in names" v-bind:key="name">
+        <td>{{ index + 1 }}</td>
+        <td>{{ name }}</td>
+        <td>
+          <button class="btn btn-sm bg-primary text-white" v-on:click="handleClick(name)"
+          v-on:mousemove="handleMouseEvent(name, $event)" v-on:mouseleave="handleMouseEvent(name, $event)">
+          Select
+          </button>
+        </td>
+      </tr>
+    </table>
+    <!-- <div class="bg-primary text-white m-2 p-3 text-center">
+      <h3 v-on:click="handleEvent">{{ name }}</h3>
+    </div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "EventPageComponent",
+    data: function() {
+      return {
+        counter: 0,
+        message: "Ready",
+        names: ["Life Jacket", "Kayak", "Soccer Ball", "Stadium"]
+      }
+    },
+    methods: {
+      // handleEvent($event) {
+      //   this.name = $event.type;
+      // },
+      handleClick(name) {
+        this.message = `Select: ${name}`;
+      },
+      handleMouseEvent(name, $event) {
+        if ($event.type == "mousemove") {
+          this.message = `Move in ${name} ${this.counter++}`;
+        } else {
+          this.counter = 0;
+          this.message = "Ready";
+        }
+      }
+    }
+  }
+</script>
--- templatesanddata/src/main.js
+++ templatesanddata/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 // import App from './App.vue'
 // import Product from './components/Product.vue'
-import PagedProduct from './components/PagedProduct.vue'
+//import PagedProduct from './components/PagedProduct.vue'
+import EventPage from './components/EventPage.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(PagedProduct),
+  render: h => h(EventPage),
 }).$mount('#app')
