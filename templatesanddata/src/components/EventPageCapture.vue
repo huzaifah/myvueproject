2189b1f250f65c4d309d5695e7bfd4cbadbefa79
Huzaifah <huzaifahd@hotmail.com>
2019-04-02 11:42:42 +0800

    Added event stop

--- templatesanddata/src/components/EventPageCapture.vue
+++ templatesanddata/src/components/EventPageCapture.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click.capture="handleClick">
             Outer element
-            <div id="middle-element" class="bg-secondary p-4" v-on:click.self="handleClick">
+            <div id="middle-element" class="bg-secondary p-4" v-on:click.stop="handleClick">
                 Middle element
                 <div id="inner-element" class="bg-info p-4" v-on:click="handleClick">
                 Inner element
