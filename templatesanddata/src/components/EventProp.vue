b392b9c5ae2fd12d58840bed36c343fc72683194
Huzaifah <huzaifahd@hotmail.com>
2019-04-02 10:41:07 +0800

    Added event propagation

---
+++ templatesanddata/src/components/EventProp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-fluid">
+        <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click="handleClick">
+            Outer element
+            <div id="middle-element" class="bg-secondary p-4" v-on:click="handleClick">
+                Middle element
+                <div id="inner-element" class="bg-info p-4" v-on:click="handleClick">
+                Inner element
+                </div>
+            </div>         
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyComponent",
+        methods: {
+            handleClick($event) {
+                console.log(`handleClick target: ${$event.target.id}` + ` currentTarget: ${$event.currentTarget.id}`);
+            }
+        }
+    }
+</script>
--- templatesanddata/src/main.js
+++ templatesanddata/src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 // import App from './App.vue'
 // import Product from './components/Product.vue'
 //import PagedProduct from './components/PagedProduct.vue'
-import EventPage from './components/EventPage.vue'
+//import EventPage from './components/EventPage.vue'
+import Page from './components/EventProp.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(EventPage),
+  render: h => h(Page),
 }).$mount('#app')
