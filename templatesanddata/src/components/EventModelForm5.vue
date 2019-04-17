b84f9a97f9fcdc1a369b450b1495f4e25067cd01
Huzaifah <huzaifahd@hotmail.com>
2019-04-17 16:15:52 +0800

    Implemented trimming whitespace

---
+++ templatesanddata/src/components/EventModelForm5.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-fluid">
+        <div class="bg-info m-2 p-2 text-white">
+            <div>Name: **{{name}}**</div>
+        </div>
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" v-model.trim="name" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyComponent",
+    data: function() {
+        return {
+            name: "Bob"
+        }
+    }
+}
+</script>
+
