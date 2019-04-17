8777fb69dfea9de69783cf78b85da92dab178a73
Huzaifah <huzaifahd@hotmail.com>
2019-04-17 15:18:59 +0800

    Added property binding

---
+++ templatesanddata/src/components/EventModelForm3.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-fluid">
+        <div class="bg-info m-2 p-2 text-white">
+            <div>Name: {{ name }}</div>
+        </div>
+        <div class="bg-primary m-2 p-2 text-white">
+            <div class="form-check" v-for="n in allNames" v-bind:key="n">
+                <input class="form-check-input" type="radio" v-model="name" v-bind:value="n" />
+                <label class="form-check-label">{{ n }}</label>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyComponent",
+    data: function() {
+        return {
+            allNames: ["Bob", "Alice", "Joe"],
+            name: "Bob"
+        }
+    }
+}
+</script>
