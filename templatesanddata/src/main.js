0a157876216d7651ec48dbf1896701277c398903
Huzaifah <huzaifahd@hotmail.com>
2019-04-19 09:41:00 +0800

    Added model binding to dropdown

--- templatesanddata/src/main.js
+++ templatesanddata/src/main.js
@@ -13,7 +13,8 @@ import Vue from 'vue'
 //import Page from './components/EventModelForm4.vue';
 //import Page from './components/EventModelForm5.vue';
 //import Page from './components/EventModelForm6.vue';
-import Page from './components/EventModelForm7.vue';
+//import Page from './components/EventModelForm7.vue';
+import Page from './components/EventModelForm8.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
