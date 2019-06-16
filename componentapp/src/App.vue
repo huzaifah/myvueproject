29a140d9ed5c53239fdde8d46023d89a7f4aa111
Huzaifah Dzulkifli <huzaifahd@hotmail.com>
2019-06-16 19:15:47 +0800

    Created child component

---
+++ componentapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-secondary text-white text-center m-2 p-2 h5">
+    Root Component
+    <ChildComponent></ChildComponent>
+    <child-component></child-component>
+    <my-feature></my-feature>
+    
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./components/Child";
+export default {
+  name: 'App',
+  components: {
+    MyFeature: ChildComponent
+  }
+}
+</script>
---
+++ componentapp/src/components/Child.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-primary text-white text-center m-2 p-3 h6">
+    Child Component
+  </div>
+</template>
+
